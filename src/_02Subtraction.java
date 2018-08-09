456b83524b50d4574cdb90d16d7eb42397683efb
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-09 01:43:37 -0500

    creating examples of subtraction - operators

---
+++ src/_02Subtraction.java
@@ -0,0 +1,19 @@
+/**
+ * Class to show examples of subtraction operators
+ */
+public class _02Subtraction {
+    public static void main(String[] args) {
+        int i1 = 12;
+        int i2 = 5;
+        float f1 = 5.0f;
+        double d1 = 5.0;
+
+        int res1 = i1 - i2; // 7
+        float res2 = i1 - f1; // 7.0
+        double res3 = i1 - d1; // 7.0
+
+        System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+        System.out.println("res3 = " + res3);
+    }
+}
