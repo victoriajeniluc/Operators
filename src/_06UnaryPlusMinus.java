e225e3724bada093c4dc7cb533a94e443ff2f942
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-09 22:41:10 -0500

    creating examples of unary - operators

---
+++ src/_06UnaryPlusMinus.java
@@ -0,0 +1,19 @@
+/**
+ * Class to show examples of Unary Plus/ Minus
+ * @author Udayan Khattry
+ */
+
+public class _06UnaryPlusMinus {
+    public static void main(String[] args) {
+        int i1 = +10; // optional, numbers are positive w/o this
+        int i2 = 10;
+        int i3 = -100; // "-" makes it a negative number
+        int i4 = -(i1 + i2);
+
+        System.out.println("i1 = " + i1);
+        System.out.println("i2 = " + i2);
+        System.out.println("i3 = " + i3);
+        System.out.println("i4 = " + i4);
+
+    }
+}
