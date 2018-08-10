e0cd315e2d20857a7b45eb489ac55215ab1484e3
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-09 19:54:13 -0500

    creating examples of division - operators

---
+++ src/_04Division.java
@@ -0,0 +1,19 @@
+/**
+ * Class to show examples of division
+ */
+public class _04Division {
+    public static void main(String[] args) {
+        int i1 = 12;
+        int i2 = 5;
+        float f1 = 5.0f;
+        double d1 = 5.0;
+
+        int res1 = i1 / i2;
+        float res2 = i1 / f1;
+        double res3 = i1 / d1;
+
+        System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+        System.out.println("res3 = " + res3);
+    }
+}
