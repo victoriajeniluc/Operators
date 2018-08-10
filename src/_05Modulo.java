d30435bec34c32915850e0f1e82f802905d87480
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-09 19:57:59 -0500

    creating examples of modulo - operators

---
+++ src/_05Modulo.java
@@ -0,0 +1,20 @@
+/**
+ * Class to show examples for modulo
+ */
+public class _05Modulo {
+    public static void main(String[] args) {
+        int i1 = 12;
+        int i2 = 5;
+        float f1 = 5.0f;
+        double d1 = 5.0;
+
+        int res1 = i1 % i2;
+        float res2 = i1 % f1;
+        double res3 = i1 % d1;
+
+        System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+        System.out.println("res3 = " + res3);
+
+    }
+}
